fc1cc15104b984a09bfe915fea85964c259bd4d5
Ivan Herman <ivan@ivan-herman.net>
2017-05-09 13:28:07 +0200

    - preset possibility added to the HTML - updated the nicknames' files a bit

---
+++ CGI/driver/presets.js
@@ -0,0 +1,62 @@
+var presets = {
+	"dpub" 	: {
+		"group"	    : "dpub",
+		"nicknames" : "/Users/ivan/W3C/github/scribejs/test/dpub-nicknames.json",
+		"torepo"	: false,
+		"ghrepo"	: "dpub"
+	},
+	"pwg"	: {
+		"group"	    : "pwg",
+		"nicknames" : "/Users/ivan/W3C/github/scribejs/test/dpub-nicknames.json",
+		"torepo"	: false,
+		"ghrepo"	: "pwg"
+	},
+	"pbg"	: {
+		"group"	    : "pbg",
+		"nicknames" : "/Users/ivan/W3C/github/scribejs/test/dpub-nicknames.json",
+		"torepo"	: true,
+		"ghrepo"	: "pbg"
+	},
+	"pbgsc"	: {
+		"group"	    : "pbgsc",
+		"nicknames" : "/Users/ivan/W3C/github/scribejs/test/dpub-nicknames.json",
+		"torepo"	: false,
+		"ghrepo"	: "pbgsc"
+	}
+};
+
+/*------------------------------------------------------------------- */
+
+function set_presets(val) {
+	function zeropadding(n) {
+	    return n < 10 ? "0" + n : "" + n;
+	}
+
+	if(presets[val] !== undefined) {
+		/* set today's date */
+		var date = new Date();
+		var month = zeropadding(date.getMonth() + 1);
+		var day   = zeropadding(date.getDate());
+		var year  = date.getFullYear();
+		var date_input = document.getElementById("date");
+		date_input.value = year + "-" + month + "-" + day;
+
+		/* Go through the keys of the preset and set the relevant element accordingly */
+		for(var key in presets[val]) {
+		    var value = presets[val][key];
+
+			// 1. step: get the element that has to be modified
+			var element = document.getElementById(key);
+
+			// 2. modify the value. The 'torepo' element must be treated a bit differently
+			// the extra check is necessary to avoid problems in case the preset data has a bug...
+			if(element) {
+				if(key === "torepo") {
+					element.selectedIndex = value ? 1 : 0;
+				} else {
+					element.value = value;
+				}
+			}
+		}
+	}
+}
