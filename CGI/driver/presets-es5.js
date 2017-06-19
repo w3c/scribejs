241251d3726122422f1a436786371596e97e2713
Ivan Herman <ivan@ivan-herman.net>
2017-06-19 17:08:33 +0200

    Preset.js file downgraded to old-skool javascript

--- CGI/driver/presets-es5.js
+++ CGI/driver/presets-es5.js
@@ -1,60 +1,36 @@
-/*
-   The small module has originally been written in ES5, but has been dummed
-   down for portability.
-
-   The original code was left in a commentm except for all "let"-s that have been turned into "var"-s
-*/
-
-/** const storage_key = "scribejs_webstorage_presets"; **/
-var storage_key = "scribejs_webstorage_presets";
+const storage_key = "scribejs_webstorage_presets";
 
 /*------------------------------------------------------------------- */
 
-/** const retrieve_presets   = () => JSON.parse(localStorage.getItem(storage_key)) **/
-var retrieve_presets   = function() {
-	return JSON.parse(localStorage.getItem(storage_key));
-};
-
-/** const store_presets      = (all_presets) => { **/
-var store_presets      = function(all_presets) {
+const retrieve_presets   = () => JSON.parse(localStorage.getItem(storage_key))
+const store_presets      = (all_presets) => {
 	localStorage.setItem(storage_key, JSON.stringify(all_presets))
 	generate_preset_menu(all_presets);
 };
 
-/**
 const JEKYLL_NONE		= "none";
 const JEKYLL_MARKDOWN	= "md";
 const JEKYLL_KRAMDOWN	= "kd";
 
 const boolean_keys = ["torepo", "final"];
-**/
-var JEKYLL_NONE		= "none";
-var JEKYLL_MARKDOWN	= "md";
-var JEKYLL_KRAMDOWN	= "kd";
-
-var boolean_keys = ["torepo", "final"];
 
 /*
  *
  */
 function set_presets(val) {
-	/** const zeropadding = (n) =>  n < 10 ? "0" + n : "" + n; **/
-	var zeropadding = function zeropadding(n) {
-		return n < 10 ? "0" + n : "" + n;
-	};
+	const zeropadding = (n) =>  n < 10 ? "0" + n : "" + n;
 	document.getElementById('main_form').reset();
 	if(val !== "None") {
-		var all_presets = retrieve_presets();
+		let all_presets = retrieve_presets();
 		if( !_.isEmpty(all_presets) ) {
 			if( all_presets[val] !== undefined ) {
-				var preset       = all_presets[val];
-				var date         = new Date();
-				var month        = zeropadding(date.getMonth() + 1);
-				var day          = zeropadding(date.getDate());
-				var year         = date.getFullYear();
-				var date_input   = document.getElementById("date");
-				/** date_input.value = `${year}-${month}-${day}`; **/
-				date_input.value = year + "-" + month + "-" + day;
+				let preset       = all_presets[val];
+				let date         = new Date();
+				let month        = zeropadding(date.getMonth() + 1);
+				let day          = zeropadding(date.getDate());
+				let year         = date.getFullYear();
+				let date_input   = document.getElementById("date");
+				date_input.value = `${year}-${month}-${day}`;
 
 				/* Go through the keys of the preset and set the relevant element accordingly */
 				for(var key in preset) {
@@ -85,7 +61,7 @@ function set_presets(val) {
  * Reset the preset menu...
  */
 function reset_preset_menu() {
-	var presets = document.getElementById("presets");
+	let presets = document.getElementById("presets");
 	presets.selectedIndex = 0;
 }
 
@@ -93,11 +69,11 @@ function reset_preset_menu() {
  * Generate preset menu
  */
 function generate_preset_menu(all_presets) {
-	var select_element = document.getElementById("presets");
+	let select_element = document.getElementById("presets");
 	// Clean the element
 	select_element.innerHTML = "";
 	// Add an option for "None"
-	var none_element = document.createElement("option");
+	let none_element = document.createElement("option");
 	none_element.textContent = "None";
 	none_element.setAttribute("value", "None");
 	none_element.setAttribute("selected", "True");
@@ -106,9 +82,9 @@ function generate_preset_menu(all_presets) {
 	if(_.isEmpty(all_presets)) {
 		console.log("No Presets");
 	} else {
-		_.forEach(all_presets, function(value,key) {
-			var descr          = value.fullname;
-			var option_element = document.createElement("option");
+		_.forEach(all_presets, (value,key) => {
+			let descr          = value.fullname;
+			let option_element = document.createElement("option");
 			option_element.textContent = descr;
 			option_element.setAttribute("value", key);
 			select_element.appendChild(option_element);
@@ -116,9 +92,8 @@ function generate_preset_menu(all_presets) {
 	}
 }
 
-/** window.onload = () => { **/
-window.onload = function() {
-	var all_presets = localStorage.getItem(storage_key);
+window.onload = () => {
+	let all_presets = localStorage.getItem(storage_key);
 	if(all_presets) {
 		generate_preset_menu(JSON.parse(all_presets));
 	} else {
@@ -131,7 +106,7 @@ window.onload = function() {
  */
 function list_presets() {
 	console.log("--- Presets");
-	var all_presets = retrieve_presets();
+	let all_presets = retrieve_presets();
 	_.forEach(get_presets(), (value,key) => {
 		console.log(value);
 	})
@@ -143,8 +118,8 @@ function list_presets() {
  * Delete a preset
  */
 function remove_preset() {
-	var all_presets = retrieve_presets();
-	var group = document.getElementById("group").value;
+	let all_presets = retrieve_presets();
+	let group = document.getElementById("group").value;
 	store_presets(_.omit(all_presets, group));
 }
 
@@ -159,15 +134,15 @@ function clear_presets() {
  * Load data using web storage...
  */
 function store_preset() {
-	var to_be_stored = {};
+	let to_be_stored = {};
 	/* Get group name; this is used to as a key to the local storage */
-	var group = document.getElementById("group").value;
+	let group = document.getElementById("group").value;
 	if(group !== "") {
-		var targets = ["group", "nicknames", "ghrepo", "ghpath", "ghbranch", "ghname", "ghemail", "ghtoken", "fullname"];
-		_.forEach(targets, function(key) {
-			var el = document.getElementById(key);
+		let targets = ["group", "nicknames", "ghrepo", "ghpath", "ghbranch", "ghname", "ghemail", "ghtoken", "fullname"];
+		_.forEach(targets, (key) => {
+			let el = document.getElementById(key);
 			if(el) {
-				var val = el.value;
+				let val = el.value;
 				if(val !== "") {
 					to_be_stored[key] = val;
 				}
@@ -178,7 +153,7 @@ function store_preset() {
 			to_be_stored.fullname = group;
 		}
 
-		_.forEach(boolean_keys, function(key) {
+		_.forEach(boolean_keys, (key) => {
 			var element = document.getElementById(key);
 			if(element) {
 				to_be_stored[key] = element.selectedIndex === 1 ? true : false;
@@ -189,7 +164,7 @@ function store_preset() {
 		to_be_stored.jekyll = element.selectedIndex === 1 ? JEKYLL_MARKDOWN : (element.selectedIndex === 2 ? JEKYLL_KRAMDOWN: JEKYLL_NONE);
 
 		/* Here comes the meat... */
-		var all_presets = retrieve_presets();
+		let all_presets = retrieve_presets();
 		all_presets[group] = to_be_stored;
 		store_presets(all_presets);
 	} else {
@@ -207,31 +182,25 @@ function store_preset() {
  * `https://cors-anywhere.herokuapp.com` trick, and I may have to come back to this later.
  */
 function load_log() {
-	set_input_url = function(date, group) {
-		/** let [year, month, day] = date.split('-'); **/
-		var date$split = date.split('-');
-		var year  = date$split[0],
-		    month = date$split[1],
-			day   = date$split[2];
+	set_input_url = (date, group) => {
+		let [year, month, day] = date.split('-');
 		// return `https://www.w3.org/${year}/${month}/${day}-${group}-irc.txt`;
-		/** return `https://cors-anywhere.herokuapp.com/https://www.w3.org/${year}/${month}/${day}-${group}-irc.txt`; **/
-		return "https://cors-anywhere.herokuapp.com/https://www.w3.org/" + year + "/" + month + "/" + day + "-" + group + "-irc.txt";
+		return `https://cors-anywhere.herokuapp.com/https://www.w3.org/${year}/${month}/${day}-${group}-irc.txt`;
 	};
 
-	var group = document.getElementById("group").value;
-	var date  = document.getElementById("date").value;
+	let group = document.getElementById("group").value;
+	let date  = document.getElementById("date").value;
 
 	// Do it only if it is meaningful...
 	if(group !== "" && date !== undefined && date !== "") {
-		var target = document.getElementById("text");
-		var url = set_input_url(date,group);
+		let target = document.getElementById("text");
+		let url = set_input_url(date,group);
 		fetch(url)
 			.then((response) => {
 				if(response.ok) {
 					return response.text()
 				} else {
-					/* throw new Error(`HTTP response ${response.status}: ${response.statusText}`); */
-					throw new Error("HTTP response " + response.status + ": " + response.statusText);
+					throw new Error(`HTTP response ${response.status}: ${response.statusText}`);
 				}
 			})
 			.then((body) => {
@@ -239,8 +208,7 @@ function load_log() {
 				target.value = body;
 			})
 			.catch((err) => {
-				/** let message = `Problem accessing remote file ${url}: ${err.message}`; **/
-				var message = "Problem accessing remote file " + url + ": " + err.message;
+				let message = `Problem accessing remote file ${url}: ${err.message}`;
 				alert(message);
 				// reject(message);
 			});
