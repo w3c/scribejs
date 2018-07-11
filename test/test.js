971227ee01d844bfba3a06c21959d1e1af180a86
Benjamin Young <byoung2@wiley.com>
2018-07-11 12:55:35 -0400

    Improve test/test.js code styles

--- test/test.js
+++ test/test.js
@@ -1,41 +1,42 @@
+/* eslint-env node, mocha */
+
 'use strict';
 
 const assert = require('assert');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
 
 const invokeScribeJS = (params = [], handlers = null) => {
     const fixedParams = params;
     fixedParams.unshift('.');
     const proc = spawn('npx', fixedParams);
-    for (let event in handlers)
-        if ('data' === event) {
+    // TODO: fix "over looping" -- use Object.keys|.values|.entries instead
+    for (const event in handlers) {
+        if (event === 'data') {
             proc.stdout.on(event, handlers[event]);
             proc.stderr.on(event, handlers[event]);
-        } else
+        } else {
             proc.on(event, handlers[event]);
+        }
+    }
 };
 
 describe('Basics', () => {
-
     describe('Node.js', () => {
-
         it('exists', (done) => {
-            invokeScribeJS([], {exit: () => done()});
+            invokeScribeJS([], { exit: () => done() });
         });
-
     });
 
     describe('scribejs', () => {
-
         it('returns an error with no params', (done) => {
             const codeChecker = (code) => {
                 assert.strictEqual(code, 255);
                 done();
             };
-            invokeScribeJS([], {exit: codeChecker});
+            invokeScribeJS([], { exit: codeChecker });
         });
 
         it('dumps help with “-h”', (done) => {
@@ -46,19 +47,16 @@ describe('Basics', () => {
             const codeChecker = (code) => {
                 assert.ok(/Usage:/.test(output));
                 assert.ok(/Options:/.test(output));
-                assert.ok(/\-\-output\ \[output\]/.test(output));
+                assert.ok(/--output \[output\]/.test(output));
                 assert.strictEqual(code, 0);
                 done();
             };
-            invokeScribeJS(['-h'], {data: outputChecker, exit: codeChecker});
+            invokeScribeJS(['-h'], { data: outputChecker, exit: codeChecker });
         });
-
     });
-
 });
 
 describe('CLI usage', () => {
-
     const inputFilename = path.join('test', 'dpub-test.txt');
     const outputFilename = path.join(os.tmpdir(), `scribejs-${process.pid}.md`);
 
@@ -68,39 +66,37 @@ describe('CLI usage', () => {
             output += data;
         };
         const codeChecker = (code) => {
-            assert.ok(/^\!\[W3C\ Logo\]\(https:\/\/www\.w3\.org\/Icons\/w3c_home\)$/m.test(output));
-            assert.ok(/^#\s.+\s—\sMinutes$/m.test(output));
+            assert.ok(/^!\[W3C Logo\]\(https:\/\/www\.w3\.org\/Icons\/w3c_home\)$/m.test(output), 'Logo check failed');
+            assert.ok(/^#\s.+\s—\sMinutes$/m.test(output), 'Minutes header check failed');
             assert.strictEqual(code, 0);
             done();
         };
-        invokeScribeJS([inputFilename], {data: outputChecker, exit: codeChecker});
+        invokeScribeJS([inputFilename], { data: outputChecker, exit: codeChecker });
     });
 
     it('can dump result to a local file', (done) => {
         const resultChecker = (code) => {
             assert.strictEqual(code, 0);
             fs.access(outputFilename, fs.constants.F_OK | fs.constants.R_OK, (err) => {
-                if (err)
+                if (err) {
                     done(err);
-                else
-                    fs.readFile(outputFilename, (err, data) => {
-                        if (err)
-                            done(err);
-                        fs.unlink(outputFilename, (err) => {
-                            if (err)
-                                done(err);
+                } else {
+                    fs.readFile(outputFilename, (readfile_err, data) => {
+                        if (readfile_err) done(readfile_err);
+                        fs.unlink(outputFilename, (unlink_err) => {
+                            if (unlink_err) done(unlink_err);
                         });
-                        assert.ok(/^\!\[W3C\ Logo\]\(https:\/\/www\.w3\.org\/Icons\/w3c_home\)$/m.test(data));
+                        assert.ok(/^!\[W3C Logo\]\(https:\/\/www\.w3\.org\/Icons\/w3c_home\)$/m.test(data));
                         assert.ok(/^#\s.+\s—\sMinutes$/m.test(data));
                         done();
                     });
+                }
             });
         };
-        invokeScribeJS(['-o', outputFilename, inputFilename], {exit: resultChecker});
+        invokeScribeJS(['-o', outputFilename, inputFilename], { exit: resultChecker });
     });
 
     // [TODO]
-
 });
 
 describe('CGI usage', () => {
