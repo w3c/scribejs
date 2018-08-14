05b08df241aa2727d65c6053c4a8bfee68cafb7b
tripu <t@tripu.info>
2018-08-14 15:39:48 +0200

    Enable REST interfacte

--- CGI/cgi-conf.js
+++ CGI/cgi-conf.js
@@ -28,59 +28,39 @@ let default_config = {
  * - user configuration, ie, ~/.scribejs.json
  * - configuration file provided CGI call (forwarded in the argument)
  *
- * @returns {object} - full configuration. See the overal manual for the field definitions
+ * @param {Function} cb - callback, with the signature (err, data)
+ *                        where 'data' is full configuration;
+ *                        see the overal manual for the field definitions
  */
-exports.get_config = (cgi_config, script_name) => {
-	/***********************************************************************/
-	// See if there is user level config file
-	let local_config = conf_tools.json_conf_file(path.join(path.dirname(script_name), user_config_name), false);
-
-	/***********************************************************************/
-	// Combine the configuration in increasing priority order
-	let retval = _.extend(default_config, cgi_config, local_config);
-
-	/***********************************************************************/
-	// Fifths step: sanity check and some cleanup on the configuration object
-
-	// 1. If the group is provided and no explicit input, we should retreive the
-	// IRC log from the W3C site
-	// Note, however, if the group is set the IRC URL is to be set in any case,
-	// because that is the original one stored on the Web site,
-	// ie, that must be referred to in the header of the minutes
-	if(retval.group) {
-		// Set the default IRC URL
-		retval.orig_irc_log = conf_tools.set_input_url(retval.date, retval.group);
-		if(!retval.irclog) {
-			retval.input = retval.orig_irc_log
-		}
-	}
-
-	// 2. get read of the 'moment' object and use ISO date instead
-	retval.date = retval.date.format("YYYY-MM-DD");
-
-	// 3. if the input is not set, nothing should happen!
-	if(!retval.input && !retval.irclog) {
-		// There is nothing to do!!
-		throw new Error("no irc log is provided; either it should be uploaded or a date should be provided!");
-	}
-
-	// 4. if the github repo should be used, some values are required. If they are
-	// present, the output file name for the repo can be generated, if needed
-	// The name may be used even if the github storage is not in effect (as a return parameter in the header)...
-	retval.ghfname   = retval.output ? retval.output : `${retval.date}-minutes.md`
-	if(retval.torepo) {
-		let needed = [retval.ghname, retval.ghemail, retval.ghtoken, retval.ghrepo, retval.ghpath]
-		if(_.every(needed, (val) => !_.isUndefined(val))) {
-			retval.ghmessage = `Added minutes for ${retval.date} at ${moment().format("YYYY-MM-DD H:m:s Z")}`
-		} else {
-			let message = "repository output is required, but not all values are provided.\n"
-			let message2 = "are needed: ghname, ghemail, ghtoken, ghrepo, ghpath\n"
-			if(retval.ghtoken !== undefined) retval.ghtoken = "(hidden)";
-			if(retval.ghemail !== undefined) retval.ghemail = "(hidden)";
-			if(retval.ghname !== undefined)  retval.ghname  = "(hidden)";
-			throw new Error(message + message2 + JSON.stringify(retval, null, 2));
-		}
-	}
-
-	return retval
-}
+exports.get_config = (cgi_config, script_name, cb) => {
+	conf_tools.json_conf_file(path.join(path.dirname(script_name), user_config_name), false, (err, local_config) => {
+        if(err)
+            cb(err);
+        else {
+        	let retval = _.extend(default_config, cgi_config, local_config);
+        	if(retval.group) {
+        		retval.orig_irc_log = conf_tools.set_input_url(retval.date, retval.group);
+        		if(!retval.irclog)
+        			retval.input = retval.orig_irc_log
+        	}
+        	retval.date = retval.date.format("YYYY-MM-DD");
+        	if(!retval.input && !retval.irclog)
+        		throw new Error("no irc log is provided; either it should be uploaded or a date should be provided!");
+        	retval.ghfname   = retval.output ? retval.output : `${retval.date}-minutes.md`
+        	if(retval.torepo) {
+        		let needed = [retval.ghname, retval.ghemail, retval.ghtoken, retval.ghrepo, retval.ghpath]
+        		if(_.every(needed, (val) => !_.isUndefined(val)))
+        			retval.ghmessage = `Added minutes for ${retval.date} at ${moment().format("YYYY-MM-DD H:m:s Z")}`
+        		else {
+        			let message = "repository output is required, but not all values are provided.\n"
+        			let message2 = "are needed: ghname, ghemail, ghtoken, ghrepo, ghpath\n"
+        			if(retval.ghtoken !== undefined) retval.ghtoken = "(hidden)";
+        			if(retval.ghemail !== undefined) retval.ghemail = "(hidden)";
+        			if(retval.ghname !== undefined)  retval.ghname  = "(hidden)";
+        			throw new Error(message + message2 + JSON.stringify(retval, null, 2));
+        		}
+        	}
+        	cb(null, retval);
+        }
+    });
+};
--- main.js
+++ main.js
@@ -1,42 +1,77 @@
-#!/usr/bin/env node
-
 'use strict';
 
 /**
- * Convert W3C’s RRSAgent IRC bot output into minutes in Markdown
- *
- * @version: 1.1.0
- * @author: Ivan Herman, <ivan@w3.org> (https://www.w3.org/People/Ivan/)
- * @license: W3C Software License <https://www.w3.org/Consortium/Legal/2002/copyright-software-20021231>
- */
+* Convert W3C’s RRSAgent IRC bot output into minutes in Markdown
+*
+* @version: 2.0.0
+* @author: Ivan Herman, <ivan@w3.org> (https://www.w3.org/People/Ivan/)
+* @license: W3C Software License <https://www.w3.org/Consortium/Legal/2002/copyright-software-20021231>
+*/
 
-const debug   = false;
+const debug = true;
+const port  = 3000;
+
+const express = require('express');
+
 const io      = require('./lib/io');
 const convert = require('./lib/convert');
 const conf    = require('./lib/conf');
 
-let schemas = {};
 try {
-    schemas = require('./lib/schemas'); // eslint-disable-line global-require
-} catch (err) {
-    console.error(`Scribejs ${err}`);
-    // process.exit();
-}
+    // Collect and combine the configuration file
+    // Note that the get_config method is synchronous
+    // (uses a sync version of file system access)
+    let config = conf.get_config();
 
-/* This is just the overall driver of the script... */
+    if (debug) {
+        console.log(JSON.stringify(config, null, 2));
+    }
 
-async function main() {
-    try {
-        // Collect and combine the configuration file
-        // Note that the get_config method is synchronous
-        // (uses a sync version of file system access)
-        const config = conf.get_config();
-        if (debug) {
-            console.log(JSON.stringify(config, null, 2));
+    if (config.server) {
+        const app = new express();
+        app.set('case sensitive routing', true);
+        app.set('strict routing', true);
+        app.set('view cache', false);
+        app.get('*', (req, res) => {
+            console.dir(req.query);
+            config = conf.get_params(req.query);
+            let schemas = {};
+            try {
+                schemas = require('./lib/schemas'); // eslint-disable-line global-require
+            } catch (err) {
+                console.error(`Scribejs ${err}`);
+            }
+            config.nicks = io.get_nick_mapping(config);
+            const valid = schemas.validate_nicknames(config.nicks);
+            if (!valid) {
+                console.warn(`Warning: scribejs validation error in nicknames:\n${schemas.validation_errors(schemas.validate_nicknames)}`);
+                console.warn('(nicknames ignored)');
+                config.nicks = [];
+            }
+            const irc_log = io.get_irc_log(config);
+            const minutes = convert.to_markdown(irc_log, config);
+            const message = io.output_minutes(minutes, config);
+            console.log(message);
+            res.send(JSON.stringify(config, null, 4));
+        });
+        app.on('error', (err) => {
+            if (err)
+                console.error('Error while trying to launch the server: “' + err + '”.');
+        });
+        app.listen(port);
+    } else {
+
+        let schemas = {};
+
+        try {
+            schemas = require('./lib/schemas'); // eslint-disable-line global-require
+        } catch (err) {
+            console.error(`Scribejs ${err}`);
+            // process.exit();
         }
 
         // Get the nickname mappings object. The result gets added to the configuration
-        config.nicks = await io.get_nick_mapping(config);
+        config.nicks = io.get_nick_mapping(config);
 
         // Validate the nickname mapping object against the appropriate JSON schema
         const valid = schemas.validate_nicknames(config.nicks);
@@ -47,21 +82,19 @@ async function main() {
         }
 
         // Get the IRC log itself
-        const irc_log = await io.get_irc_log(config);
+        const irc_log = io.get_irc_log(config);
 
         // The main step: convert the IRC log into a markdown text
         const minutes = convert.to_markdown(irc_log, config);
 
         // Either upload the minutes to Github or dump into a local file
-        const message = await io.output_minutes(minutes, config);
+        const message = io.output_minutes(minutes, config);
 
         // That is it, folks!
         console.log(message);
-    } catch (err) {
-        console.error(`Scribejs ${err}`);
-        process.exit(255);
     }
-}
 
-// Do it!
-main();
+} catch (err) {
+    console.error(`Scribejs ${err}`);
+    process.exit(255);
+}
